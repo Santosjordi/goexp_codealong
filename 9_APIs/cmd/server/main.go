a89f1e9ef009e0a5a5335748cf736cf401918b6d
Jordi <jordiws@gmail.com>
2025-02-23 21:07:51 -0300

    created handlers and addedthe chi library for routing

--- 9_APIs/cmd/server/main.go
+++ 9_APIs/cmd/server/main.go
@@ -1,11 +1,42 @@
 package main
 
-import "github.com/santosjordi/posgoexp/9_apis/configs"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/santosjordi/posgoexp/9_apis/configs"
+	"github.com/santosjordi/posgoexp/9_apis/internal/entity"
+	"github.com/santosjordi/posgoexp/9_apis/internal/infra/database"
+	"github.com/santosjordi/posgoexp/9_apis/internal/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
 	config, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
-	println(config.DBDriver)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	// repository receives the db instance
+	productDB := database.NewProduct(db)
+
+	// handler receives the repo, the repo needs to have all interface methods implemented
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Post("/products", productHandler.CreateProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Get("/products", productHandler.GetAllProducts)
+
+	http.ListenAndServe(config.WebServerPort, r)
 }
