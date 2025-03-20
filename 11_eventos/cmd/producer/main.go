b37973149d6a899c12a3b08d319a68036d2922a5
Jordi <jordiws@gmail.com>
2025-03-19 21:59:49 -0300

    created a producer and a consumer for rabbitmq queues

---
+++ 11_eventos/cmd/producer/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/santosjordi/posgoexp/fcutils/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "hello world!")
+}
